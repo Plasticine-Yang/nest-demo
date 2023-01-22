0d7188b2d323115660c3506ec54c6f7c54e78313
plasticine <975036719@qq.com>
2023-01-22 20:48:45 +0800

    chore: nothing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common'
 
+import { AppService } from './app.service'
+
 @Controller()
 export class AppController {
+  constructor(private appService: AppService) {}
+
   @Get()
   sayHello() {
-    return 'hello'
+    return this.appService.getHello()
   }
 }
